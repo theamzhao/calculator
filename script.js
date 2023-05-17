7b3c04be3c7ca074b740c6800dd1e02755dcaa63
theamzhao <126366310+theamzhao@users.noreply.github.com>
2023-05-16 23:00:16 -0400

    Make behavior for evaluate as last button pushed

--- script.js
+++ script.js
@@ -2,21 +2,25 @@
 const numbers = document.querySelectorAll('.numbers button');
 const functions = document.querySelectorAll('.functions button');
 const display = document.querySelector('.display');
+const clear = document.querySelector('.functions #clear');
 display.textContent = '0';
 const calcArray = [0];
 const funcArray = ['/','*','-','+','=','Clear'];
+var evalFlag = 0;
 
 numbers.forEach((button) => button.addEventListener('click', addNumbers));
 functions.forEach((button) => button.addEventListener('click', addFunctions));
 
 // FUNCTIONS
 function addNumbers() {
-    //console.log(button.textContent);
     let endIndex = calcArray.length-1;
-    if (display.textContent == '0'){
+    if (display.textContent == '0'){ 
         display.textContent = this.textContent;
         calcArray[0] = this.textContent;
-        console.log(calcArray);
+    } else if (evalFlag == 1){
+        display.textContent = this.textContent;
+        calcArray[0] = this.textContent;
+        evalFlag = 0;
     } else if (funcArray.includes(calcArray[endIndex])){
         functions.forEach((button) => button.style.backgroundColor = '#f4f4f4');
         display.textContent = this.textContent;
@@ -35,24 +39,27 @@ function addFunctions() {
 
     if (this.textContent == "="){
         evaluateArray();
-    }
-    else if (funcArray.includes(calcArray[endIndex])){ 
+        evalFlag = 1;
+    } else if (this.textContent == 'Clear'){
+        clearAll();
+        evalFlag = 0;
+    }else if (funcArray.includes(calcArray[endIndex])){ 
         calcArray.pop();
         calcArray.push(this.textContent);
-        //console.log(this);
-        //console.log(functions)
         functions.forEach((button) => button.style.backgroundColor = '#f4f4f4');
         this.style.backgroundColor = 'orange';
         console.log(calcArray);
+        evalFlag = 0;
     } else {
         this.style.backgroundColor = 'orange';
         calcArray.push(this.textContent);
         console.log(calcArray);
+        evalFlag = 0;
     } 
 }
 
 function evaluateArray() {
-    //evaluate the function (number function number)
+
     let evaluation = '';
     calcArray.forEach((input) => {        
         if (funcArray.includes(input)){
@@ -61,10 +68,13 @@ function evaluateArray() {
             evaluation += input;
         }
     });
-    evaluation = eval(evaluation);
+    evaluation = eval(evaluation); // if this catches a decimal value, the float will start to make weird numbers
     display.textContent = evaluation;
     calcArray.forEach(() => calcArray.shift());
-    calcArray[0] = evaluation;
-    //output result into display
-    //save as the only value in the array 
+    calcArray[0] = evaluation.toString();
+}
+
+function clearAll() {
+    calcArray.forEach(() => calcArray.shift());
+    display.textContent = '0';
 }
