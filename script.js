2274bf16e68431316b6a68bcd01ebe7bbfe88815
theamzhao <126366310+theamzhao@users.noreply.github.com>
2023-05-16 22:18:51 -0400

    Refactor functions, add evaluation function

--- script.js
+++ script.js
@@ -1,55 +1,70 @@
-// HTML
+// BODY
 const numbers = document.querySelectorAll('.numbers button');
-console.log(numbers);
 const functions = document.querySelectorAll('.functions button');
 const display = document.querySelector('.display');
 display.textContent = '0';
-console.log(display.textContent);
 const calcArray = [0];
+const funcArray = ['/','*','-','+','=','Clear'];
+
+numbers.forEach((button) => button.addEventListener('click', addNumbers));
+functions.forEach((button) => button.addEventListener('click', addFunctions));
 
 // FUNCTIONS
-numbers.forEach((button) => button.addEventListener('click', () => {
+function addNumbers() {
     //console.log(button.textContent);
+    let endIndex = calcArray.length-1;
     if (display.textContent == '0'){
-        display.textContent = button.textContent;
-        calcArray[0] = parseInt(button.textContent);
+        display.textContent = this.textContent;
+        calcArray[0] = this.textContent;
+        console.log(calcArray);
+    } else if (funcArray.includes(calcArray[endIndex])){
+        functions.forEach((button) => button.style.backgroundColor = '#f4f4f4');
+        display.textContent = this.textContent;
+        calcArray.push(this.textContent);
         console.log(calcArray);
     } else {
-        display.textContent += button.textContent;
-        calcArray.push(parseInt(button.textContent));
+        display.textContent += this.textContent;
+        calcArray[endIndex] += this.textContent;
         console.log(calcArray);
     }
-})); 
+}
 
-functions.forEach((button) => button.addEventListener('click', () => {
-    console.log(button.textContent);
-    if (typeof calcArray[calcArray.length-1] == 'number'){ // if this is the only one
-        button.style.backgroundColor = 'orange';
-        calcArray.push(button.textContent);
+function addFunctions() {
+    let endIndex = calcArray.length-1;
+    console.log(this.textContent);
+
+    if (this.textContent == "="){
+        evaluateArray();
+    }
+    else if (funcArray.includes(calcArray[endIndex])){ 
+        calcArray.pop();
+        calcArray.push(this.textContent);
+        //console.log(this);
+        //console.log(functions)
+        functions.forEach((button) => button.style.backgroundColor = '#f4f4f4');
+        this.style.backgroundColor = 'orange';
         console.log(calcArray);
     } else {
-        calcArray.pop();
-        calcArray.push(button.textContent);
-        //functions.style.backgroundColor = '#f4f4f4';
-        button.style.backgroundColor = 'blue';
+        this.style.backgroundColor = 'orange';
+        calcArray.push(this.textContent);
         console.log(calcArray);
     } 
-})); 
+}
 
-// Make most recently selected function orange
-// un-orange other function buttons
-
-// if a number is selected
-//    array length == 1 && array[0] = 0, clear 0 and push number
-//    if last number is a function, then push
-//    if last number is a number, push
-//
-// push numbers to array
-//
-// when a function is selected &&
-//    last value in array is function, then replace with selected function
-//    last value in array is number, push to array 
-// if equal is pressed, then 
-//    concatenate all numbers between functions 
-//    apply function to 
-
+function evaluateArray() {
+    //evaluate the function (number function number)
+    let evaluation = '';
+    calcArray.forEach((input) => {        
+        if (funcArray.includes(input)){
+            evaluation += input;
+        } else {
+            evaluation += input;
+        }
+    });
+    evaluation = eval(evaluation);
+    display.textContent = evaluation;
+    calcArray.forEach(() => calcArray.shift());
+    calcArray[0] = evaluation;
+    //output result into display
+    //save as the only value in the array 
+}
