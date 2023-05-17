ca47ff4e163299fcc0d89ddfb7afaf8f6409e100
theamzhao <126366310+theamzhao@users.noreply.github.com>
2023-05-16 23:38:56 -0400

    Format calculator style, additional buttons

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const numbers = document.querySelectorAll('.numbers button');
 const functions = document.querySelectorAll('.functions button');
 const display = document.querySelector('.display');
-const clear = document.querySelector('.functions #clear');
+const clear = document.querySelector('#clear');
 display.textContent = '0';
 const calcArray = [0];
 const funcArray = ['/','*','-','+','=','Clear'];
@@ -10,6 +10,7 @@ var evalFlag = 0;
 
 numbers.forEach((button) => button.addEventListener('click', addNumbers));
 functions.forEach((button) => button.addEventListener('click', addFunctions));
+clear.addEventListener('click',clearAll);
 
 // FUNCTIONS
 function addNumbers() {
@@ -17,12 +18,14 @@ function addNumbers() {
     if (display.textContent == '0'){ 
         display.textContent = this.textContent;
         calcArray[0] = this.textContent;
+    } else if (this.textContent == 'C'){
+        // add one more case if a function is clicked and current value is 0 to keep 0
     } else if (evalFlag == 1){
         display.textContent = this.textContent;
         calcArray[0] = this.textContent;
         evalFlag = 0;
     } else if (funcArray.includes(calcArray[endIndex])){
-        functions.forEach((button) => button.style.backgroundColor = '#f4f4f4');
+        functions.forEach((button) => button.style.backgroundColor = '#ffa500');
         display.textContent = this.textContent;
         calcArray.push(this.textContent);
         console.log(calcArray);
@@ -40,18 +43,17 @@ function addFunctions() {
     if (this.textContent == "="){
         evaluateArray();
         evalFlag = 1;
-    } else if (this.textContent == 'Clear'){
-        clearAll();
-        evalFlag = 0;
-    }else if (funcArray.includes(calcArray[endIndex])){ 
+    } else if (funcArray.includes(calcArray[endIndex])){ 
         calcArray.pop();
         calcArray.push(this.textContent);
-        functions.forEach((button) => button.style.backgroundColor = '#f4f4f4');
-        this.style.backgroundColor = 'orange';
+        functions.forEach((button) => button.style.backgroundColor = '#ffa500');
+        this.style.backgroundColor = '#ffffff';
+        this.style.color = '#ffa500';
         console.log(calcArray);
         evalFlag = 0;
     } else {
-        this.style.backgroundColor = 'orange';
+        this.style.backgroundColor = '#ffffff';
+        this.style.color = '#ffa500';
         calcArray.push(this.textContent);
         console.log(calcArray);
         evalFlag = 0;
